f58728454135e74534cf21b3b541569fc4e838d0
Maynard Peralta <maynperalta@gmail.com>
2025-12-10 21:47:15 -0700

    Moved scanner from recursive method to main

--- CSC372Module5/src/main/Main.java
+++ CSC372Module5/src/main/Main.java
@@ -5,19 +5,24 @@ import java.util.ArrayList;
 
 public class Main {
 	ArrayList<Double> userList = new ArrayList<Double>();
-	public static void product(double num, int userNum, double userProduct) {
-		Scanner scnr = new Scanner(System.in);
+	public static void product(int userNum, double userProduct, Scanner scnr) {
+		
 		if (userNum < 5) {
 			System.out.println("Please enter a number: ");
-			num = scnr.nextDouble();
+			double num = scnr.nextDouble();
+			
 			userProduct *= num;
-			userNum ++;
-			product(num, userNum, userProduct);
+			
+			product(userNum + 1, userProduct, scnr);
 		} else {
 			System.out.println("Operation complete. Product of inputs is: " + userProduct);
 		}
 	}	
 	public static void main(String[] args) {
-		product(0, 0, 1);	
+		Scanner scnr = new Scanner(System.in);
+		
+		product(0, 1, scnr);	
+		
+		scnr.close();
 	}
 }

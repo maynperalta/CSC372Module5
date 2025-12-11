c4509875a61c5830ce6fa56bb0930aa45e01e202
Maynard Peralta <maynperalta@gmail.com>
2025-12-10 21:17:18 -0700

    First draft

--- CSC372Module5/src/main/Main.java
+++ CSC372Module5/src/main/Main.java
@@ -1,10 +1,23 @@
 package main;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
-
+	ArrayList<Double> userList = new ArrayList<Double>();
+	public static void product(double num, int userNum, double userProduct) {
+		Scanner scnr = new Scanner(System.in);
+		if (userNum <= 5) {
+			System.out.println("Please enter a number: ");
+			num = scnr.nextDouble();
+			userProduct *= num;
+			userNum ++;
+			product(num, userNum, userProduct);
+		} else {
+			System.out.println("Operation complete. Product of inputs is: " + userProduct);
+		}
+	}	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		product(0, 0, 0);	
 	}
-
 }
